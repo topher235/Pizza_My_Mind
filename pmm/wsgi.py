87d9ac3e3fdeac70e3402bff604d9fc27884bbc6
Christopher Hurt <topherhurt@gmail.com>
2018-05-20 14:02:04 -0400

    Update wsgi.py

--- pmm/wsgi.py
+++ pmm/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pmm.settings")
-os.environ.setdefault("ADMIN_EMAIL", "raymond.sutton.15@cnu.edu")
+os.environ.setdefault("ADMIN_EMAIL", "clarem@cnu.edu")
 os.environ.setdefault("PMM_EMAIL", "mindmypizza@gmail.com")
 os.environ.setdefault("PMM_HOST", "smtp.gmail.com")
 os.environ.setdefault("PMM_PASS", "pizzaPIZZA")
